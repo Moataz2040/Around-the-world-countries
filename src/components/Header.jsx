8508c7c4e0b2278a10603e20ff75928c2c9cd1ed
Moataz <motazosama1111@gmail.com>
2025-06-07 16:21:24 +0300

    fix deployment issue 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import Logo from "./logo";
+import Logo from "./Logo";
 import ThemeSwithcer from "./ThemeSwithcer";
 const Header = () => {
   return (
-    <header className="mb-6 bg-white shadow md:mb-12 dark:bg-gray-800">
+    <header className="mb-6 bg-white shadow dark:bg-gray-800 md:mb-12">
       <div className="container mx-auto px-5 md:mx-0">
         <div className="flex h-20 items-center justify-between">
           <Logo />
